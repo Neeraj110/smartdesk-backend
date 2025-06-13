b18560a69142db955a6c32cee3e2688c700edaf9
Neeraj gaur <neerajgaur8448@gmail.com>
2025-06-13 19:46:49 +0530

    fix: update CORS origin to production URL

--- index.ts
+++ index.ts
@@ -17,7 +17,8 @@ const PORT = process.env.PORT || 3000;
 connectDB();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "https://smartdesk-frontend-howv.vercel.app",
+    // origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
