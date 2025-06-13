aa5003c42f1d0840401b18ff99141561e2f09a66
Neeraj gaur <neerajgaur8448@gmail.com>
2025-06-13 19:40:49 +0530

    set SameSite attribute to 'none' in cookie options

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -11,6 +11,7 @@ const options = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
   httpOnly: true,
   secure: true,
+  sameSite: "none",
 };
 
 const createToken = (user: IUserDocument) => {
