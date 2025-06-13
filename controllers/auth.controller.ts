5a921f437550a5f7a5cb9a73c40fddefd1db4491
Neeraj gaur <neerajgaur8448@gmail.com>
2025-06-13 19:41:32 +0530

    refactor: enforce type assertion for SameSite attribute in cookie options

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -11,7 +11,7 @@ const options = {
   expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
   httpOnly: true,
   secure: true,
-  sameSite: "none",
+  sameSite: "none" as const,
 };
 
 const createToken = (user: IUserDocument) => {
