f7c458e4007b823b75402f8242c4156f1c8f4f7d
Neeraj gaur <neerajgaur8448@gmail.com>
2025-11-23 16:39:07 +0530

    fix: update multer storage configuration and add .gitkeep to temp directory

--- middleware/multer.middleware.ts
+++ middleware/multer.middleware.ts
@@ -1,13 +1,23 @@
 import multer from "multer";
 
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "./public/temp");
+//   },
+
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + file.originalname);
+//   },
+// });
+
+// export const upload = multer({ storage: storage });
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "./public/temp");
+    cb(null, "/tmp");
   },
-
   filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
-
 export const upload = multer({ storage: storage });
