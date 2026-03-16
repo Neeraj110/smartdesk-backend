43971a2f51d970a206634dbd2ece42db6ab564e4
Neeraj gaur <neerajgaur8448@gmail.com>
2026-03-16 18:48:05 +0530

    fix: simplify CORS origin configuration and remove CLIENT_URL check

--- index.ts
+++ index.ts
@@ -12,16 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 connectDB();
-const url = process.env.CLIENT_URL!;
-if (!url) {
-  throw new Error("CLIENT_URL is not defined in .env file");
-}
 app.use(
   cors({
-    origin: url,
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  })
+  }),
 );
 
 app.use(express.json());
